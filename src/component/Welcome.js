b8bd565c46778a73635ae3bc653e55432c11185f
pketanp <pketanp01@gmail.com>
2020-03-27 20:56:17 +0530

    update

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Welcome extends Component {
   render() {
     const { name, aka } = this.props;
+    //const { state1, state2 } = this.state;
     return (
       <h1>
         Welcome {name} A.K.A {aka}{" "}
